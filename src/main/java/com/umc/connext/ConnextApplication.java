42eada5af09660e82e20369ada08b54015db720d
Yoonji Lee <estelle0329@ewha.ac.kr>
2026-01-02 19:46:37 +0900

    init: spring initializer 추가 및 gradle 수정

---
+++ src/main/java/com/umc/connext/ConnextApplication.java
@@ -0,0 +1,13 @@
+package com.umc.connext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnextApplication.class, args);
+	}
+
+}
