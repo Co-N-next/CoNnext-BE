d4109f9510f6a556c42f2deccb4de82e5ab355a3
Yoonji Lee <estelle0329@ewha.ac.kr>
2026-01-26 14:42:08 +0900

    docs: swagger 배포서버 추가

--- src/main/java/com/umc/connext/common/config/SwaggerConfig.java
+++ src/main/java/com/umc/connext/common/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,15 +19,17 @@ public class SwaggerConfig {
                         .title("Connext API")
                         .description("공연장 관련 서비스")
                         .version("v1.0.0"))
-                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                .addServersItem(new Server()
+                        .url("https://api.con-next.xyz")
+                        .description("Production"))
+                .addServersItem(new Server()
                         .url("http://localhost:8080")
-                        .description("Local"))
+                        .description("Local"));
 //                .addSecurityItem(new SecurityRequirement().addList("JWT"))
 //                .components(new Components()
 //                        .addSecuritySchemes("JWT", new SecurityScheme()
 //                                .type(SecurityScheme.Type.HTTP)
 //                                .scheme("bearer")
-//                                .bearerFormat("JWT")))
-                                ;
+//                                .bearerFormat("JWT")));
     }
 }
