a192bfaa34573eb5bc338c019c38a71874317bba
LeeJuYoung12 <rkfmgm@naver.com>
2026-02-02 20:54:24 +0900

    fix: ObjectMapper import 관련 수정

--- src/main/java/com/umc/connext/global/auth/util/SecurityResponseWriter.java
+++ src/main/java/com/umc/connext/global/auth/util/SecurityResponseWriter.java
@@ -4,7 +4,7 @@ import com.umc.connext.common.response.Response;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
--- src/main/java/com/umc/connext/global/oauth2/handler/CustomOAuth2FailureHandler.java
+++ src/main/java/com/umc/connext/global/oauth2/handler/CustomOAuth2FailureHandler.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
