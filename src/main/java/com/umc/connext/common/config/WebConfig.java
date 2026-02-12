9aed3966dc8f1375d6dc8472437edd0791140d45
Yoonji Lee <estelle0329@ewha.ac.kr>
2026-02-12 15:04:58 +0900

    chore: webmvcconfig 사용 안하도록 수정

--- src/main/java/com/umc/connext/common/config/WebConfig.java
+++ src/main/java/com/umc/connext/common/config/WebConfig.java
@@ -6,12 +6,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://api.con-next.xyz")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+
 }
